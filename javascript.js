42db59701cb210ff6489195b4ad06c431e22fba0
noblepups <scprice2@uh.edu>
2017-04-17 21:42:29 -0500

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,31 @@
+/**
+ * Created by noblepups on 4/16/2017.
+ */
+/**
+body=document.querySelector("body");
+isPurple = false;
+button= document.getElementById("button");
+button.addEventListener("click", function () {
+    if (isPurple){
+        document.body.style.background = "white";
+        isPurple = false;
+    } else {
+        document.body.style.background = "purple";
+        isPurple = true;
+    }
+
+});
+
+function myFunction() {
+    var btn = document.createElement("BUTTON");
+    var t = document.createTextNode("CLICK ME");
+    btn.appendChild(t);
+    document.body.appendChild(btn);
+}
+ **/
+ $("div").css.style = ""
+
+
+
+
+
